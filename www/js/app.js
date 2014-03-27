c2a25e54ff47ff0df2cff01e77f48dd651b442ae
fwolf <fwolf@bu.edu>
2014-03-27 09:59:21 -0400

    removed angular-mobile-nav

--- www/js/app.js
+++ www/js/app.js
@@ -7,19 +7,20 @@ function jsonp_callback(data) {
 
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ajoslin.mobile-navigate','ngMobile'])
-    .config(function ($compileProvider){
-        $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-    })
+var myApp = angular.module('myApp', ['ngRoute', 'ngTouch', 'myApp.filters', 'myApp.services', 'myApp.directives'])
+    // .config(function ($compileProvider){
+    //     $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|filterse|tel):/);
+    //     console.log('here');
+    // })
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
-        $routeProvider.when('/view1', {templateUrl: 'partials/notificationView.html'});
-        $routeProvider.when('/view2', {templateUrl: 'partials/geolocationView.html'});
-        $routeProvider.when('/view3', {templateUrl: 'partials/accelerometerView.html'});
-        $routeProvider.when('/view4', {templateUrl: 'partials/deviceInfoView.html'});
-        $routeProvider.when('/view5', {templateUrl: 'partials/cameraView.html'});
-        $routeProvider.when('/view6', {templateUrl: 'partials/contactsView.html'});
-        $routeProvider.when('/view7', {templateUrl: 'partials/compassView.html'});
-        $routeProvider.when('/view8', {templateUrl: 'partials/hackerNewsView.html'});
+        // $routeProvider.when('/view1', {templateUrl: 'partials/notificationView.html'});
+        // $routeProvider.when('/view2', {templateUrl: 'partials/geolocationView.html'});
+        // $routeProvider.when('/view3', {templateUrl: 'partials/accelerometerView.html'});
+        // $routeProvider.when('/view4', {templateUrl: 'partials/deviceInfoView.html'});
+        // $routeProvider.when('/view5', {templateUrl: 'partials/cameraView.html'});
+        // $routeProvider.when('/view6', {templateUrl: 'partials/contactsView.html'});
+        // $routeProvider.when('/view7', {templateUrl: 'partials/compassView.html'});
+        // $routeProvider.when('/view8', {templateUrl: 'partials/hackerNewsView.html'});
         $routeProvider.otherwise({redirectTo: '/'});
   }]);
