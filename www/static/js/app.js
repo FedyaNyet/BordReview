5b025e3d83f0b7c3c79ee2e6eebf12fdc31ed2f6
fwolf <fwolf@bu.edu>
2014-03-29 17:58:12 -0400

    fixed things up

--- www/static/js/app.js
+++ www/static/js/app.js
@@ -1,26 +1,19 @@
 'use strict';
 
-function jsonp_callback(data) {
-    // returning from async callbacks is (generally) meaningless
-    console.log(data.found);
-}
-
-
-// Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['ngRoute', 'ngTouch', 'myApp.filters', 'myApp.services', 'myApp.directives'])
-    // .config(function ($compileProvider){
-    //     $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|filterse|tel):/);
-    //     console.log('here');
-    // })
+// Declare app level module
+var myApp = angular
+    .module('myApp', ['ngRoute', 'ngTouch', 'myApp.services'])
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'templates/homeView.html', controller: 'HomeCtrl'});
-        // $routeProvider.when('/view1', {templateUrl: 'templates/notificationView.html'});
-        // $routeProvider.when('/view2', {templateUrl: 'templates/geolocationView.html'});
-        // $routeProvider.when('/view3', {templateUrl: 'templates/accelerometerView.html'});
-        // $routeProvider.when('/view4', {templateUrl: 'templates/deviceInfoView.html'});
-        // $routeProvider.when('/view5', {templateUrl: 'templates/cameraView.html'});
-        // $routeProvider.when('/view6', {templateUrl: 'templates/contactsView.html'});
-        // $routeProvider.when('/view7', {templateUrl: 'templates/compassView.html'});
-        // $routeProvider.when('/view8', {templateUrl: 'templates/hackerNewsView.html'});
+        $routeProvider.when('/', {templateUrl: 'templates/listView.html', controller: 'ListCtrl'});
         $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+    }]);
+//     .config('indexedDBProvider', function ($indexedDBProvider) {
+//         $indexedDBProvider
+//             .connection('myIndexedDB')
+//             .upgradeDatabase(myVersion, function(event, db, tx){
+//                 var objStore = db.createObjectStore('people', {keyPath: 'ssn'});
+//                 objStore.createIndex('name_idx', 'name', {unique: false});
+//                 objStore.createIndex('age_idx', 'age', {unique: false});
+//                 console.log('ready');
+//             });
+//       });
--- www/static/js/controllers.js
+++ www/static/js/controllers.js
@@ -1,22 +1,25 @@
 'use strict';
 
-function HomeCtrl($scope,$rootScope) {
-    $rootScope.showSettings = false;
-    $scope.slidePage = function (path,type) {
-        return true;
-        // navSvc.slidePage(path,type);
-    };
-    $scope.back = function () {
-        return true;
-        // navSvc.back();
-    };
-    $scope.changeSettings = function () {
-        $rootScope.showSettings = true;
-    };
-    $scope.closeOverlay = function () {
+myApp.controller('ListCtrl',
+    ['$rootScope', '$scope', 'cardsService', function ($rootScope, $scope, cardsService) {
+        
         $rootScope.showSettings = false;
-    };
-}
+
+        $scope.slidePage = function (path,type) {
+            return true;
+            // navSvc.slidePage(path,type);
+        };
+        $scope.back = function () {
+            return true;
+            // navSvc.back();
+        };
+        $scope.changeSettings = function () {
+            $rootScope.showSettings = true;
+        };
+        $scope.closeOverlay = function () {
+            $rootScope.showSettings = false;
+        };
+}]);
 
 
 
