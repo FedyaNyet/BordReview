c7377d26257208a3d61304ca96ff89174d3b8834
Fyodor Wolf <fwolf@bu.edu>
2014-03-31 22:16:42 -0400

    got webSQL storage working.

--- www/static/js/controllers.js
+++ www/static/js/controllers.js
@@ -1,48 +1,32 @@
 'use strict';
 
-myApp.controller('NavController',['$rootScope','$scope', '$location', function($rootScope, $scope, $location){
+myApp.controller('NavController',['$rootScope','$scope', '$location', 'cardsService', function($rootScope, $scope, $location, cardsService){
 
-    
+    cardsService.init();
+
     $rootScope.toggleNav = function(){
         $('.side-nav').toggleClass('left-nav');
     };
-    
     $scope.isActive = function(route) {
-        console.log(route,$location.path());
         return route === $location.path();
     }
 }]);
 
 myApp.controller('ListCtrl',
-    ['$rootScope', '$scope', 'cardsService', function ($rootScope, $scope, cardsService) {
+    ['$scope', 'cardsService', function ($scope, cardsService) {
         
-        $scope.cards = [
-            {id: 0, question: "What is?", answer: "an ansswer!"},
-            {id: 1, question: "What is it not?", answer: "an question..."},
-            {id: 2, question: "Where is it?", answer: "MY HOUSE!"},
-            {id: 3, question: "What is?", answer: "an ansswer!"},
-            {id: 4, question: "What is it not?", answer: "an question..."},
-            {id: 5, question: "Where is it?", answer: "MY HOUSE!"},
-            {id: 6, question: "What is?", answer: "an ansswer!"},
-            {id: 7, question: "What is it not?", answer: "an question..."},
-            {id: 8, question: "Where is it?", answer: "MY HOUSE!"},
-            {id: 9, question: "What is?", answer: "an ansswer!"},
-            {id: 10, question: "What is it not?", answer: "an question..."},
-            {id: 11, question: "Where is it?", answer: "MY HOUSE!"},
-            {id: 12, question: "What is?", answer: "an ansswer!"},
-            {id: 13, question: "What is it not?", answer: "an question..."},
-            {id: 14, question: "Where is it?", answer: "MY HOUSE!"},
-        ];
+        $scope.cards = [];
+        cardsService.readAll().then(function (results) {
+            $scope.cards = [];
+            for(var i = 0; i < results.rows.length; i++){
+                $scope.cards.push(results.rows.item(i));
+            }
+        });
+
         $scope.getListItemClass = function($index){
             if ($index == 0) return "topcoat-list__item--first";
             return "topcoat-list__item";
         }
-
-        // Todo.readAll().then(function (todos) {
-        //     $scope.$apply(function () {
-        //         $scope.todos = todos;
-        //     });
-        // });
 }]);
 
 
--- www/static/js/services.js
+++ www/static/js/services.js
@@ -1,5 +1,46 @@
 'use strict';
 
-myApp.factory('cardsService',[function(){
-	console.log('giving cards!');
+myApp.factory('cardsService',['$q',function($q){
+
+	var db;
+	var fixture = {
+		1: {question:'My name is what?', 			answer:'Slim Shady', 	status: 1},
+		2: {question:'Is there anybody in there?',  answer:'no, nobody.', 	status: 0}
+	}
+
+	return {
+		init:function(){
+			db = openDatabase('NeuroRad', '1.0', 'NeuroRad', 2 * 1024 * 1024);
+			if(!localStorage.getItem('created')){
+				db.transaction(function (tx) {
+					var queries = [
+						"CREATE TABLE `card` (`id` int(11) NOT NULL, `question` varchar(255), `answer` varchar(255), `status` int(2), PRIMARY KEY (`id`))",
+						"CREATE TABLE `photo` (`id` int(11) NOT NULL,`path` varchar(50), `position` int(5),`card_id` int(11) NOT NULL, PRIMARY KEY (`id`))"
+					];
+					for(var id in fixture){
+						var card = fixture[id];
+						queries.push("INSERT INTO `card` (`id`, `question`, `answer`, `status`) VALUES ("+ id + ",\"" + card.question +"\",\""+ card.answer + "\","+ card.status +")");
+					}
+					for(var idx in queries){
+						var query = queries[idx];
+						console.log(query);
+						tx.executeSql(query);
+					}
+					localStorage.setItem('created','true');
+				});
+			}
+		},
+		readAll:function(){
+			var deferred = $q.defer();
+            db.transaction(function(tx) {
+            	tx.executeSql("SELECT * FROM `card`",[],function(tx, rs){
+                    deferred.resolve(rs); 
+            	},function(){
+                	console.log('ere');
+                    deferred.reject("Error, please re-run the application!"); 
+                });
+            });
+            return deferred.promise;
+	    },
+	};
 }])
