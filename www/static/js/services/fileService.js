3df066acee9a9da0eb36e0523e3b05368164ecd8
fwolf <fwolf@bu.edu>
2014-04-03 11:05:27 -0400

    added fail callback.

--- www/static/js/services/fileService.js
+++ www/static/js/services/fileService.js
@@ -10,24 +10,32 @@ myApp.factory('fileService',['$q',function($q){
             window.requestFileSystem(LocalFileSystem.PERSISTENT, 2*1024*1024, function(fs){
                 console.log("GOT FS", fs);
                 var filename = url.substr(url.lastIndexOf("/")+1);
-                fs.root.getFile("derp.txt", {create: true}, function(file) {
-                    console.log(file);
-                    var sPath = file.fullPath.replace("derp.txt","");
-                    // file.remove(); //remove it so we can use the name
-                    (new FileTransfer()).download(
-                        url,
-                        sPath + filename,
-                        function(theFile) {
-                            console.log("download complete: " + theFile.toURI());
-                            promise.resolve(theFile.toURI())
-                        },
-                        function(error) {
-                            console.log("download error source " + error.source);
-                            console.log("download error target " + error.target);
-                            console.log("upload error code: " + error.code);
-                        }
-                    );
-                });
+                fs.root.getFile("derp.txt", {create: true}, 
+                    function(file) {
+                        console.log(file);
+                        var sPath = file.fullPath.replace("derp.txt","");
+                        // file.remove(); //remove it so we can use the name
+                        (new FileTransfer()).download(
+                            url,
+                            sPath + filename,
+                            function(theFile) {
+                                console.log("download complete: " + theFile.toURI());
+                                promise.resolve(theFile.toURI())
+                            },
+                            function(error) {
+                                console.log("download error source " + error.source);
+                                console.log("download error target " + error.target);
+                                console.log("upload error code: " + error.code);
+                            }
+                        );
+                    }, function(){
+                        console.log("can't get file..");
+                        deferred.reject();
+                    }
+                );
+            },function(){
+                console.log("Can't get FS");
+                deferred.reject();
             });
             return deferred.promise;
         } 
